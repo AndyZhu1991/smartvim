f6f8000607a522d487d0346a81adfd601f5b8735
ZhuJinchang <stdzhu@gmail.com>
2015-03-15 11:02:41 +0800

    Some improvement

--- smartvim.py
+++ smartvim.py
@@ -8,33 +8,26 @@ def convert_grep_file_arg(grep_file_arg) :
     arg_len = len(grep_file_arg)
     
     if grep_file_arg[arg_len-1] != ':' :
-        return grep_file_arg
+        return (grep_file_arg, )
     
     i = arg_len - 2
     while i >= 0 :
         if grep_file_arg[i] == ':' :
             break
         i -= 1
-    if i == -1 :
-        return grep_file_arg
+    if i <= 0 :
+        return (grep_file_arg, )
 
     try:
         line = int(grep_file_arg[i+1: arg_len-1])
     except ValueError :
-        return grep_file_arg
+        return (grep_file_arg, )
     else:
-        return grep_file_arg[0: i] + " +" + str(line)
-
-
-def get_origin_command(argv) :
-    result = ""
-    for arg in argv :
-        result += arg + " "
-    return result
+        return (grep_file_arg[0: i], "+" + str(line))
 
 
 if __name__  == "__main__" :
     if len(sys.argv) == 2 :
-        os.system("vim " + convert_grep_file_arg(sys.argv[1]))
+        os.execvp("vim", ("vim", ) + convert_grep_file_arg(sys.argv[1]))
     else:
-        os.system("vim " + get_origin_command(sys.argv[1:]))
+        os.execvp("vim", ("vim", ) + sys.argv[1:])
